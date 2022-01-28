f0d94aa3f46bfcd4bf956b6e27f6941b9377f335
choidoorim <doorim321@naver.com>
2022-01-29 02:11:39 +0900

    update: amountFor 함수분리

--- ch1/example-one.js
+++ ch1/example-one.js
@@ -24,32 +24,39 @@ const plays = {
     "othello": {"name": "Othello", "type":  "tragedy"}
 }
 
-function statement(invoice, plays) {
+const amountFor = (perf, play) => {
+    let thisAmount = 0;
+
+    switch (play.type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if(perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience
+            break;
+        default:
+            throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+
+    return thisAmount;
+}
+
+const statement = (invoice, plays) => {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
     for (const perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if(perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience
-                break;
-            default:
-               throw new Error(`알 수 없는 장르: ${play.type}`);
-        }
+        // thisAmount 의 값을 추출할 때는 amountFor 메서드를 사용한다.
+        let thisAmount = amountFor(perf, play);
         // 포인트 적립
         volumeCredits += Math.max(perf.audience - 30, 0);
         // 희극 관객 5명마다 추가 포인트를 제공
