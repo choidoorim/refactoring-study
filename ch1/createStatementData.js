25f958dea540ef7034fab5753e94dc0cb2e648e0
choidoorim <doorim321@naver.com>
2022-02-17 22:31:38 +0900

    update: 공연료 계산 코드 계산기 클래스 안으로 복사

--- ch1/createStatementData.js
+++ ch1/createStatementData.js
@@ -4,6 +4,30 @@ class PerformanceCalculator {
       this.performance = aPerformance;
       this.play = aPlay;
     }
+
+    get amount() {
+        let result = 0;
+
+        switch (this.play.type) {
+            case "tragedy":
+                result = 40000;
+                if (this.performance.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if(this.performance.audience > 20) {
+                    result += 10000 + 500 * (this.performance.audience - 20);
+                }
+                result += 300 * this.performance.audience
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${this.play.type}`);
+        }
+
+        return result;
+    }
 }
 
 export default function createStatementData(invoice, plays) {
